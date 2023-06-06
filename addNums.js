6fe7bf5240d3bfd47b1b67cae6a6a6feb449c969
John Wilkins <80089266+wilkinsjohnstanley@users.noreply.github.com>
2023-06-06 12:09:01 +0900

    Update addNums.js

--- addNums.js
+++ addNums.js
@@ -18,3 +18,9 @@ const  addNums = (num1, num2) => {
 
 }
 console.log(addNums(5, 5));
+
+//Arrow Function 2 without return statement
+const  addNums = (num1, num2) => num1+num2;
+
+
+console.log(addNums(5, 5)); 
