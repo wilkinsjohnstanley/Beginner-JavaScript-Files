a42bd0e848e76f5da647c42eea73abb53fc8d273
John Wilkins <80089266+wilkinsjohnstanley@users.noreply.github.com>
2023-06-06 11:58:54 +0900

    Create SwitchStatements.js

---
+++ SwitchStatements.js
@@ -0,0 +1,13 @@
+const x = 1;
+const color = x>10?'red':'blue';
+switch(color){
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'blue':
+        console.log('color is blue');
+        break;
+    default:
+        console.log('color is NOT red or blue');
+        break;
+}
