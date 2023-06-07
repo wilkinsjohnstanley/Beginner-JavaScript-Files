3254fed7517c5c2fbfcacd9fc72d08c528418ffe
John Wilkins <80089266+wilkinsjohnstanley@users.noreply.github.com>
2023-06-07 13:12:32 +0900

    Create ternaryOperators.java

---
+++ ternaryOperators.java
@@ -0,0 +1,19 @@
+//ternary operators
+
+let age=28;
+//if 27, his name is John. If greater, it's John Sr
+// let name= age>27 &&"John";
+// let name2=age>25 || "John";
+
+let age2=9;
+let name = age2> 10? "Pedro":"Pepper Jack";
+
+
+// if (age>10){
+//     name = "John"
+// } else{
+//     name = "John Sr"
+// }
+
+
+console.log(name);
