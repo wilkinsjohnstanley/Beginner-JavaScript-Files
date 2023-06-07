f8d2ae3449d21459af831f6da5571d4aac6e71f3
John Wilkins <80089266+wilkinsjohnstanley@users.noreply.github.com>
2023-06-07 13:20:11 +0900

    Create pepperJack.java

---
+++ pepperJack.java
@@ -0,0 +1,12 @@
+const cheese ={
+    name: "Pepper Jack",
+    age: 2,
+    isCheese: true,
+};
+
+// const name = cheese.name
+// const age = cheese.age
+// const isCheese =cheese.isCheese
+
+//do it in a single line by using the destructuring property of objects
+const {name, age, isCheese} = cheese;
