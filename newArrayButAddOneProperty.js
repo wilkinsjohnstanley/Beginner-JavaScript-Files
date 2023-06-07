3a3b1136c9ecc0ffbfcedf1478f9063889eef816
John Wilkins <80089266+wilkinsjohnstanley@users.noreply.github.com>
2023-06-07 13:31:58 +0900

    Create newArrayButAddOneProperty.js

---
+++ newArrayButAddOneProperty.js
@@ -0,0 +1,6 @@
+const gourmetCheese =["Brie", "Camembert", "Ricotta", "Neufchatel", "Feta", "Gorgonzola", "Quark", "Danish Blue", "Asiago", "Queso Blanco", "Grana Padano", "Stilton", "Appenzeller", "Raclette", "Roquefort"];
+//Oh, I forgot one!
+const gourmetCheese2=[...gourmetCheese, "Cheese Cake"];
+
+console.log(gourmetCheese)
+console.log(gourmetCheese2)
